3d084115f53ff2e67cc318618129f9f23ff9d3e8
johurul-abir <mdjohurulislam555@gmail.com>
2024-07-14 23:11:29 +0600

    build add

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   server: {
     port: 3000,
   },
+  build: {
+    rollupOptions: {
+      input: "client/src/main.jsx", // Specify the correct entry JavaScript file here
+    },
+  },
 });
